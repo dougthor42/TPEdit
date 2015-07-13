d190438cc09b1cbbd10747063758ec1dccb4f0c4
Douglas Thor <dougthor42@users.noreply.github.com>
2015-07-13 16:15:10 -0700

    Update to build_exe.bat, added log folder to git

--- build_exe.bat
+++ build_exe.bat
@@ -1,4 +1,6 @@
 call C:\WinPython27\Scripts\env.bat
+cd C:\WinPython27\projects\github\TPEdit
 RMDIR /S /Q .\build\exe.win32-2.7 
-%WINPYDIR%\python.exe build_executables.py build
-.\build\exe.win32-2.7\main.exe
+python build_executables.py build
+cd .\build\exe.win32-2.7
+main.exe
---
+++ log/README.txt
@@ -0,0 +1,3 @@
+This folder contains log files for TPedit.
+
+Each log file will span 7 days, rolling over at midnight on Sundays.
